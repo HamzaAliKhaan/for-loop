83b7b81344d7af2b2df17c285487d583c00457dd
alihamza <154751641+HamzaAliKhaan@users.noreply.github.com>
2024-12-16 02:17:37 +0500

    Add files via upload

---
+++ document.js
@@ -0,0 +1,14 @@
+////////////////////// Loop ///////////////////////
+////////For Loop statement//////////////////////
+document.write("<h3>This Is Even Number</h3> <br>")
+for(let i = 1; i <= 50; i++){
+    if(i % 2 == 0){
+        document.write(`Even Number ${i}<br>`)
+    }
+}
+document.write("<h3>This Is Odd Number</h3> <br>")
+for(let i = 1; i <=50; i++){
+    if(i % 2 != 0){
+    document.write(`odd Number  ${i} <br>`)
+    }
+}
